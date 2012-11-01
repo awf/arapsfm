81734ec1fd590b3828648cba4b43bf80650104c3
Richard Stebbing <richie.stebbing@gmail.com>
2012-11-01 12:07:13 +0000

    ** WIP ** Add inplace_cheetah1B_Cheetah4.py

---
+++ inplace_cheetah1B_Cheetah4.py
@@ -0,0 +1,48 @@
+# inplace_cheetah1B_Cheetah4.py
+
+# Imports
+from os import system
+
+# SINGLE_ARAP_CMD
+SINGLE_ARAP_CMD = '''python solve_single.py "single_arap_proj" \
+data/models/Cheetah_4.npz \
+data/user_constraints/cheetah1B/{frame_index}.npz \
+--use_linear_transform "1.0,1.0" \
+--solver_options "dict(maxIterations=200, improvementThreshold=1e-6)" \
+--input_frame data/frames/cheetah1B/{frame_index}.png \
+--output cheetah1B/Cheetah_4_{frame_index}_0.dat \
+> cheetah1B/Cheetah_4_{frame_index}_0.log'''
+
+# main_single_arap
+def main_single_arap():
+    # for all frame indices
+    for i in xrange(16):
+        cmd = SINGLE_ARAP_CMD.format(frame_index=i)
+        print '-> %s' % cmd
+        system(cmd)
+
+# SINGLE_SILHOUETTE_CMD
+SINGLE_SILHOUETTE_CMD = '''python generate_silhouette_info.py \
+--step 30. \
+data/models/Cheetah_4.npz \
+data/user_constraints/cheetah1B/{frame_index}.npz \
+cheetah1B/Cheetah_4_{frame_index}_Silhouette_Info.dat --use_linear_transform \
+> cheetah1B/Cheetah_4_{frame_index}_Silhouette_Info.log'''
+
+# main_single_silhouette
+def main_single_silhouette():
+    # for all frame indices
+    for i in xrange(12, 16):
+        cmd = SINGLE_SILHOUETTE_CMD.format(frame_index=i)
+        print '-> %s' % cmd
+        system(cmd)
+
+MULTIVIEW_CMD = '''
+'''
+
+# main_multiview
+def main_multiview():
+    pass
+
+if __name__ == '__main__':
+    main_single_silhouette()
