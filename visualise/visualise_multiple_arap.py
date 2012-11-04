dc937737a45c68921ba71cc8c7ad46505800830a
Richard Stebbing <richard.stebbing@sjc.ox.ac.uk>
2012-11-04 23:02:07 +0000

    Add solvers/arap.py

--- visualise/visualise_multiple_arap.py
+++ visualise/visualise_multiple_arap.py
@@ -10,73 +10,10 @@ from itertools import groupby
 from operator import itemgetter
 from geometry import quaternion as quat
 
-import scipy.sparse.linalg
-from scipy import sparse
+from solvers.arap import ARAPVertexSolver
 
 from pprint import pprint
 from matplotlib import cm
-
-# ARAPVertexSolver
-class ARAPVertexSolver(object):
-    def __init__(self, adj, W, V):
-        # Default constraints on the last index
-        C = np.r_[V.shape[0] - 1].astype(int)
-        U = V[C]
-
-        # W: weights (csr)
-        W = W.tocsr()
-
-        # N: number of vertices including the active constraint set
-        # N_F: number of vertices to solve for (free vertices)
-        N = adj.shape[0]
-        N_F = N - len(C)
-
-        # build the system matrix and solution matrix
-        b = np.zeros((N_F, U.shape[1]), dtype=np.float64)
-        L = sparse.lil_matrix((N_F, N_F), dtype=np.float64)
-
-        for i in xrange(N_F):
-            L[i,i] = W[i].sum() 
-
-            for j in adj[i]:
-                if j >= N_F:
-                    # active constraint neighbour
-                    b[i] += W[i, j] * U[j - N_F]
-                else:
-                    # free neighbour
-                    L[i,j] = -W[i,j]
-
-        # factorise the system matrix
-        self.L_solver = sparse.linalg.factorized(L.tocsc())
-
-        # save solution matrix and the reverse mappings
-        self.b = b
-        self.adj = adj
-        self.W = W
-
-        # save original vertices and updated copy
-        self.V = V
-
-    # __call__
-    def __call__(self, R):
-        # restore state
-        b = np.copy(self.b)
-        N_F = b.shape[0]
-
-        # update b
-        for i in xrange(N_F):
-            for j in self.adj[i]:
-                b[i, :] += (0.5 * self.W[i, j] * 
-                            np.dot(self.V[i] - self.V[j], 
-                            np.transpose(R[i] + R[j])))
-
-        V1 = self.V.copy()
-
-        # new vertex positions
-        for l in xrange(b.shape[1]):
-            V1[:N_F, l] = self.L_solver(b[:, l])
-
-        return V1
 
 # main
 def main():
--- visualise/visualise_single_arap.py
+++ visualise/visualise_single_arap.py
@@ -7,72 +7,9 @@ from mesh import weights
 from mesh.faces import faces_to_cell_array
 from itertools import groupby
 from operator import itemgetter
+
 from geometry import quaternion as quat
-
-import scipy.sparse.linalg
-from scipy import sparse
-
-# ARAPVertexSolver
-class ARAPVertexSolver(object):
-    def __init__(self, adj, W, V):
-        # Default constraints on the last index
-        C = np.r_[V.shape[0] - 1].astype(int)
-        U = V[C]
-
-        # W: weights (csr)
-        W = W.tocsr()
-
-        # N: number of vertices including the active constraint set
-        # N_F: number of vertices to solve for (free vertices)
-        N = adj.shape[0]
-        N_F = N - len(C)
-
-        # build the system matrix and solution matrix
-        b = np.zeros((N_F, U.shape[1]), dtype=np.float64)
-        L = sparse.lil_matrix((N_F, N_F), dtype=np.float64)
-
-        for i in xrange(N_F):
-            L[i,i] = W[i].sum() 
-
-            for j in adj[i]:
-                if j >= N_F:
-                    # active constraint neighbour
-                    b[i] += W[i, j] * U[j - N_F]
-                else:
-                    # free neighbour
-                    L[i,j] = -W[i,j]
-
-        # factorise the system matrix
-        self.L_solver = sparse.linalg.factorized(L.tocsc())
-
-        # save solution matrix and the reverse mappings
-        self.b = b
-        self.adj = adj
-        self.W = W
-
-        # save original vertices and updated copy
-        self.V = V
-
-    # __call__
-    def __call__(self, R):
-        # restore state
-        b = np.copy(self.b)
-        N_F = b.shape[0]
-
-        # update b
-        for i in xrange(N_F):
-            for j in self.adj[i]:
-                b[i, :] += (0.5 * self.W[i, j] * 
-                            np.dot(self.V[i] - self.V[j], 
-                            np.transpose(R[i] + R[j])))
-
-        V1 = self.V.copy()
-
-        # new vertex positions
-        for l in xrange(b.shape[1]):
-            V1[:N_F, l] = self.L_solver(b[:, l])
-
-        return V1
+from solvers.arap import ARAPVertexSolver
 
 # main
 def main():
@@ -97,7 +34,9 @@ def main():
     adj, W = weights.weights(V0, T_, weights_type='cotan')
     solveV0_X = ARAPVertexSolver(adj, W, V0)
 
-    V0_X = solveV0_X([quat.rotationMatrix(quat.quat(x)) for x in z['X']])
+    rotM = lambda x: quat.rotationMatrix(quat.quat(x))
+    V0_X = solveV0_X(map(rotM, z['X']))
+
     vis.add_mesh(V0_X, z['T'], actor_name='V0_X')
     lut = vis.actors['V0_X'].GetMapper().GetLookupTable()
     lut.SetTableValue(0, *int2dbl(0, 255, 0))
