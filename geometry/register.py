0c5ba05db42d321c5226a681b1b8fdacd96aa09a
Richard Stebbing <richard.stebbing@sjc.ox.ac.uk>
2012-11-07 21:59:09 +0000

    Use `leastsq` instead of `fmin_bfgs` in geometry/register.py

--- geometry/register.py
+++ geometry/register.py
@@ -3,7 +3,7 @@
 # Imports
 import numpy as np
 from scipy.linalg import block_diag
-from scipy.optimize import fmin_bfgs, leastsq
+from scipy.optimize import leastsq
 from quaternion import *
 
 # right_multiply_affine_transform
@@ -23,27 +23,16 @@ def right_multiply_affine_transform(V0, V):
     return T, d
 
 # right_multiply_rigid_transform
-def right_multiply_rigid_transform(V0, V, verbose=False):
+def right_multiply_rigid_transform(V0, V):
     x = np.r_[0., 0., 0., 1., 0., 0., 0.]
 
-    def f(x, verbose=False):
+    def func(x):
         ax, s, d = x[:3], x[3], x[4:]
         A = s * rotationMatrix(quat(ax))
         r = V - (np.dot(V0, A) + d)
-        e = np.sum(r*r)
-        if verbose:
-            print '%.3f: ' % e, np.around(x, decimals=4)
-        return e
+        return np.ravel(r)
 
-    def callback(x):
-        f(x, verbose=True)
-
-    if verbose:
-        callback = lambda x: f(x, True)
-    else:
-        callback = None
-
-    x = fmin_bfgs(f, x, callback=callback, disp=verbose)
+    x = leastsq(func, x, xtol=1e-6, ftol=1e-6, gtol=1e-6)[0]
     x, s, d = x[:3], x[3], x[4:]
 
     return (s * rotationMatrix(quat(x))), d
@@ -52,24 +41,13 @@ def right_multiply_rigid_transform(V0, V, verbose=False):
 def right_multiply_rigid_uniform_scale_transform(V0, V, verbose=False):
     x = np.r_[0., 0., 0., 0., 0., 0.]
 
-    def f(x, verbose=False):
+    def func(x):
         ax, d = x[:3], x[3:]
         A = rotationMatrix(quat(ax))
         r = V - (np.dot(V0, A) + d)
-        e = np.sum(r*r)
-        if verbose:
-            print '%.3f: ' % e, np.around(x, decimals=4)
-        return e
+        return np.ravel(r)
 
-    def callback(x):
-        f(x, verbose=True)
-
-    if verbose:
-        callback = lambda x: f(x, True)
-    else:
-        callback = None
-
-    x = fmin_bfgs(f, x, callback=callback, disp=verbose)
+    x = leastsq(func, x, xtol=1e-6, ftol=1e-6, gtol=1e-6)[0]
     x, d = x[:3], x[3:]
 
     return rotationMatrix(quat(x)), d
