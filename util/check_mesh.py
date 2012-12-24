3bfe04f68075a04a840e41f3baaeeddfda7f3312
Richard Stebbing <richard.stebbing@sjc.ox.ac.uk>
2012-12-24 09:58:13 +0000

    Extend util/check_mesh.py

--- util/check_mesh.py
+++ util/check_mesh.py
@@ -3,7 +3,7 @@
 # Imports
 import argparse
 import numpy as np
-from vtk_ import iter_vtkCellArray
+from vtk_ import iter_vtkCellArray, view_vtkPolyData, numpy_to_vtkPolyData
 
 # main
 def main():
@@ -30,6 +30,20 @@ def main():
 
     m = np.unique(map(len, edges.itervalues()))
     print '# Adjacent faces to each edge:', m
+
+    highlight = set()
+    for edge, face_indices in edges.iteritems():
+        if len(face_indices) > 2:
+            print face_indices
+            for index in face_indices:
+                map(highlight.add, faces[index])
+
+    min_, max_ = np.amin(V, axis=0), np.amax(V, axis=0)
+    sphere_radius = 0.025 * np.amin(max_ - min_)
+
+    poly_data = numpy_to_vtkPolyData(V, z['cells'])
+    view_vtkPolyData(poly_data, highlight=list(highlight), 
+                     sphere_radius=sphere_radius)
 
 if __name__ == '__main__':
     main()
