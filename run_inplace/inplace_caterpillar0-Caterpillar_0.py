ffe8f95a30c3946ee7c983a1285eb4a0e92a6999
Richard Stebbing <richie.stebbing@gmail.com>
2012-12-26 16:31:06 +0000

    Add (redundant) run_inplace/ scripts

---
+++ run_inplace/inplace_caterpillar0-Caterpillar_0.py
@@ -0,0 +1,55 @@
+# inplace_caterpillar0-Caterpillar_0.py
+
+# Imports
+import argparse
+import os, sys
+from multiprocessing import Pool, cpu_count
+from pprint import pprint
+
+# SINGLE_ARAP_CMD
+SINGLE_ARAP_CMD = '''python solve_single.py "single_arap_proj" \
+data/models/Caterpillar_0.npz \
+data/user_constraints/caterpillar0/Caterpillar_0/{i:d}.npz --use_linear_transform \
+"1.0,1.0" \
+--solver_options "dict(maxIterations=200, improvementThreshold=1e-6)" \
+--input_frame data/frames/caterpillar0/{i:d}.png \
+--output Caterpillar0-Caterpillar_0/ARAP_Proj/{i:d}.dat \
+> Caterpillar0-Caterpillar_0/ARAP_Proj/{i:d}.log'''
+
+# SINGLE_SILHOUETTE_CMD
+SINGLE_SILHOUETTE_CMD = '''python generate_silhouette_info.py \
+--step 40. \
+data/models/Caterpillar_0.npz \
+data/user_constraints/caterpillar0/Caterpillar_0/{i:d}.npz --use_linear_transform \
+Caterpillar0-Caterpillar_0/Silhouette_Info/{i:d}.npz \
+> Caterpillar0-Caterpillar_0/Silhouette_Info/{i:d}.log'''
+
+# CMD_FORMAT_LOOKUP
+CMD_FORMAT_LOOKUP = {'arap' : SINGLE_ARAP_CMD,
+                     'silhouette' : SINGLE_SILHOUETTE_CMD}
+
+# system
+def system(cmd):
+    print '[%d]:' % os.getpid(), cmd
+    os.system(cmd)
+
+# main
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('solver', choices=CMD_FORMAT_LOOKUP.keys(), type=str)
+    parser.add_argument('indices', type=str)
+    args = parser.parse_args()
+    for key in ['indices']:
+        setattr(args, key, eval(getattr(args, key)))
+
+    pprint(args)
+
+    all_cmds = map(lambda i: CMD_FORMAT_LOOKUP[args.solver].format(i=i), 
+                   args.indices)
+
+    p = Pool(cpu_count())
+    p.map(system, all_cmds)
+
+if __name__ == '__main__':
+    main()        
+    
