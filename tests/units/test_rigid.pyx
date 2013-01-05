da89e6de9fd44691bab15f8251913167d618e2e9
Richard Stebbing <richie.stebbing@gmail.com>
2013-01-05 23:32:01 +0000

    Add tests/units/test_rigid.*

---
+++ tests/units/test_rigid.pyx
@@ -0,0 +1,35 @@
+# test_rigid.pyx
+# cython:boundscheck=False
+# cython:wraparound=False
+# cython:cdivision=True
+
+# Imports
+import numpy as np
+cimport numpy as np
+np.import_array()
+
+ctypedef np.float64_t DTYPE_t
+DTYPE = np.float64
+
+cdef extern from "test_rigid.h":
+    object EvaluateRigidRegistrationEnergy_c 'EvaluateRigidRegistrationEnergy' (
+        np.ndarray npy_V0,
+        np.ndarray npy_V,
+        np.ndarray npy_s,
+        np.ndarray npy_xg,
+        np.ndarray npy_d,
+        double w,
+        int k,
+        bint debug)
+
+def EvaluateRigidRegistrationEnergy(np.ndarray[DTYPE_t, ndim=2] V0,
+                                    np.ndarray[DTYPE_t, ndim=2] V,
+                                    np.ndarray[DTYPE_t, ndim=2] s,
+                                    np.ndarray[DTYPE_t, ndim=2] xg,
+                                    np.ndarray[DTYPE_t, ndim=2] d,
+                                    DTYPE_t w,
+                                    np.int32_t k,
+                                    bint debug=False):
+    
+    return EvaluateRigidRegistrationEnergy_c(V0, V, s, xg, d, w, k, debug)
+
