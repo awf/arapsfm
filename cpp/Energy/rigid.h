d5eed0eccbcfd37f69bcb4a0592775874e2aa5c8
Richard Stebbing <richie.stebbing@gmail.com>
2013-01-06 11:35:57 +0000

    Add `BackwardRigidRegistrationEnergy` to cpp/Energy/rigid.h

--- cpp/Energy/rigid.h
+++ cpp/Energy/rigid.h
@@ -185,4 +185,128 @@ protected:
     const bool _fixedV;
 };
 
+// BackwardRigidRegistrationEnergy
+class BackwardRigidRegistrationEnergy : public Energy
+{
+public:
+    BackwardRigidRegistrationEnergy(const VertexNode & V0, const VertexNode & V,
+                                    const ScaleNode & s, const RotationNode & Xg, const VertexNode & d, 
+                                    const double w, bool fixedV)
+        : _V0(V0), _V(V), _s(s), _Xg(Xg), _d(d), _w(w), _fixedV(fixedV)
+    {}
+
+    virtual void GetCostFunctions(vector<NLSQ_CostFunction *> & costFunctions)
+    {
+        vector<int> * pUsedParamTypes = new vector<int>;
+        pUsedParamTypes->push_back(_s.GetParamId());
+        pUsedParamTypes->push_back(_Xg.GetParamId());
+        pUsedParamTypes->push_back(_d.GetParamId());
+        if (!_fixedV)
+            pUsedParamTypes->push_back(_V.GetParamId());
+
+        costFunctions.push_back(new Energy_CostFunction(*this, pUsedParamTypes, 3));
+    }
+
+    virtual int GetCorrespondingParam(const int k, const int i) const
+    {
+        switch (i)
+        {
+        case 0:
+            return _s.GetOffset();
+        case 1:
+            return _Xg.GetOffset();
+        case 2:
+            return _d.GetOffset();
+        case 3:
+            return k + _V.GetOffset();
+        default:
+            break;
+        };
+
+        assert(false);
+        return -1;
+    }
+
+    virtual int GetNumberOfMeasurements() const
+    {
+        return _V.GetCount();
+    }
+
+    virtual void EvaluateResidual(const int k, Vector<double> & e) const
+    {
+        double q[4];
+        quat_Unsafe(_Xg.GetRotation(0), q);
+
+        double R[9];
+        rotationMatrix_Unsafe(q, R);
+        double RV0[3];
+        multiply_A_v_Static<double, 3, 3>(R, _V0.GetVertex(k), RV0);
+
+        double Vd[3];
+        subtractVectors_Static<double, 3>(_V.GetVertex(k), _d.GetVertex(0), Vd);
+        scaleVectorIP_Static<double, 3>(_s.GetScale(), Vd);
+
+        subtractVectors_Static<double, 3>(Vd, RV0, &e[0]);
+        scaleVectorIP_Static<double, 3>(_w, &e[0]);
+    }
+
+    virtual void EvaluateJacobian(const int k, const int whichParam, Matrix<double> & J) const
+    {
+        switch (whichParam)
+        {
+        case 0:
+        {
+            // s
+            double Vd[3];
+            subtractVectors_Static<double, 3>(_V.GetVertex(k), _d.GetVertex(0), J[0]);
+            scaleVectorIP_Static<double, 3>(_w, J[0]);
+            return;
+        }
+        case 1:
+        {
+            // Xg
+            double q[4];
+            quat_Unsafe(_Xg.GetRotation(0), q);
+
+            double D[12];
+            quatDx_Unsafe(_Xg.GetRotation(0), D);
+
+            rigidJac_X_Unsafe(_V0.GetVertex(k), _w, q, D, J[0]);
+            return;
+        }
+        case 2:
+        {
+            // d
+            fillVector_Static<double, 9>(0., J[0]);
+            J[0][0] = - _s.GetScale() * _w;
+            J[1][1] = - _s.GetScale() * _w;
+            J[2][2] = - _s.GetScale() * _w;
+            return;
+        }
+        case 3:
+        {
+            // V
+            fillVector_Static<double, 9>(0., J[0]);
+            J[0][0] = _s.GetScale() * _w;
+            J[1][1] = _s.GetScale() * _w;
+            J[2][2] = _s.GetScale() * _w;
+            return;
+        }
+        default:
+            break;
+        }
+
+        assert(false);
+    }
+
+protected:
+    const VertexNode & _V0;
+    const VertexNode & _V;
+    const ScaleNode & _s;
+    const RotationNode & _Xg;
+    const VertexNode & _d;
+    const double _w;
+    const bool _fixedV;
+};
+
 #endif
