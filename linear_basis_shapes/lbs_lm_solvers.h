ae64ccdccbafad0d24e2d2dc79b8c3f3b232ef62
Richard Stebbing <richard.stebbing@sjc.ox.ac.uk>
2013-01-15 03:26:53 +0000

    Add linear_basis_shapes/lbs_lm_solvers.*

---
+++ linear_basis_shapes/lbs_lm_solvers.h
@@ -0,0 +1,106 @@
+#ifndef __LBS_LM_SOLVERS_H__
+#define __LBS_LM_SOLVERS_H__
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+
+#include <Math/v3d_linear.h>
+using namespace V3D;
+
+#include "Geometry/mesh.h"
+#include "Energy/laplacian.h"
+
+#include "linear_basis_shape.h"
+#include "linear_basis_shape_projection.h"
+
+#include "Solve/node.h"
+#include "Solve/problem.h"
+#include "Solve/optimiser_options.h"
+
+#include "Util/pyarray_conversion.h"
+#include <vector>
+#include <utility>
+using namespace std;
+
+// solve_single_projection
+int solve_single_projection(PyArrayObject * npy_T,
+                            PyObject * list_Vb,
+                            PyArrayObject * npy_s,
+                            PyArrayObject * npy_Xg,
+                            PyArrayObject * npy_Vd,
+                            PyArrayObject * npy_y,
+                            PyArrayObject * npy_C,
+                            PyArrayObject * npy_P,
+                            PyArrayObject * npy_lambdas,
+                            PyArrayObject * npy_preconditioners,
+                            bool debug,
+                            const OptimiserOptions * options)
+{
+    if (debug)
+        asm("int $0x3");
+
+    PYARRAY_AS_VECTOR(double, npy_preconditioners, preconditioners);
+
+    PYARRAY_AS_MATRIX(int, npy_T, T);
+    auto Vb = make_vectorOfMatrix<double>(list_Vb);
+    PYARRAY_AS_MATRIX(double, npy_s, s);
+    PYARRAY_AS_MATRIX(double, npy_Xg, Xg);
+    PYARRAY_AS_MATRIX(double, npy_Vd, Vd);
+    PYARRAY_AS_MATRIX(double, npy_y, y);
+    Mesh mesh(Vb[0].num_rows(), T);
+
+    Problem problem;
+
+    vector<VertexNode *> nodes_Vb;
+
+    for (int i = 0; i < Vb.size(); i++)
+    {
+        nodes_Vb.push_back(new VertexNode(Vb[i]));
+        problem.AddNode(nodes_Vb.back());
+
+        nodes_Vb.back()->SetPreconditioner(preconditioners[0]);
+    }
+
+    auto node_s = new ScaleNode(s);
+    node_s->SetPreconditioner(preconditioners[2]);
+    problem.AddNode(node_s); 
+
+    auto node_Xg = new RotationNode(Xg);
+    node_Xg->SetPreconditioner(preconditioners[1]);
+    problem.AddNode(node_Xg); 
+
+    auto node_Vd = new VertexNode(Vd);
+    problem.AddNode(node_Vd); 
+
+    auto node_y = new CoefficientsNode(y);
+    node_y->SetPreconditioner(preconditioners[3]);
+    problem.AddNode(node_y); 
+
+    Matrix<double> V_(Vb[0].num_rows(), Vb[0].num_cols());
+    auto node_V = new LinearBasisShapeNode(V_, nodes_Vb, *node_y, *node_s, *node_Xg, *node_Vd);
+    problem.AddCompositeNode(node_V);
+
+    PYARRAY_AS_VECTOR(int, npy_C, C);
+    PYARRAY_AS_MATRIX(double, npy_P, P);
+
+    PYARRAY_AS_VECTOR(double, npy_lambdas, lambdas);
+
+    problem.AddEnergy(new LinearBasisShapeProjectionEnergy(*node_V, C, P, 
+                      sqrt(lambdas[0])));
+
+    vector<const ScaleNode *> nodes_s;
+    nodes_s.push_back(node_s);
+
+    for (int i = 0; i < nodes_Vb.size(); i++)
+    {
+        problem.AddEnergy(new LaplacianEnergy(*nodes_Vb[i], vector<const ScaleNode *>(nodes_s),
+                                              mesh, sqrt(lambdas[1])));
+    }
+
+    int ret = problem.Minimise(*options);
+
+    return ret;
+}
+
+#endif
